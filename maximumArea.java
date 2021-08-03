25d83b643b20a3e95bd5e07fdffce981c6d19f2a
sush <59618780+sush-101@users.noreply.github.com>
2021-08-04 00:14:32 +0530

    Update maximumArea.java

--- maximumArea.java
+++ maximumArea.java
@@ -4,38 +4,26 @@ class Solution {
     public int maximalRectangle(char[][] matrix) {
         if(matrix == null || matrix.length == 0)return 0;
         int r = matrix.length, c = matrix[0].length;
-        int arr[][] = new int[r][c];
-        
-        
-        //calculate height
+        int arr[] = new int[c];
+        int res = 0;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(matrix[i][j] == '1'){
-                    arr[i][j] = 1;
-                    if(i > 0){
-                        arr[i][j] += arr[i-1][j];
-                    }
-                }else arr[i][j] = 0;
+                    arr[j]++;
+                }else arr[j] = 0;
             }
-        }
-        
-        int res = 0;
-      
-        //calculate max area
-        for(int i=0;i<r;i++){
-            res = Math.max(res, getMaxArea(arr[i]));
+            res = Math.max(res, getMaxArea(arr));
         }
         return res;
-        
     }
+    
     public int getMaxArea(int arr[]){
         int len = arr.length, left[] = new int[len], right[] = new int[len];
         left[0] = -1;right[len-1]=len;
         int res = 0;
-      
-        //left most index where elements after left are >= arr[i]
         for(int i=1;i<len;i++){
             if(arr[i] > arr[i-1])left[i] = i-1;
+            else if(arr[i] == arr[i-1])left[i] = left[i-1];
             else{
                 int cur = left[i-1];
                 while(cur != -1 && arr[cur] >= arr[i]){
@@ -44,10 +32,9 @@ class Solution {
                 left[i] = cur;
             }
         }
-      
-        //right most index where elements before right are >= arr[i]
         for(int i=len-2;i>=0;i--){
             if(arr[i] > arr[i+1])right[i] = i+1;
+            else if(arr[i] == arr[i+1])right[i] = right[i+1];
             else{
                 int cur = right[i+1];
                 while(cur != len && arr[cur] >= arr[i]){
