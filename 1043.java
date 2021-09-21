de3871ee27e91cbbcc6de3a787a6079521a5ffb5
sush <59618780+sush-101@users.noreply.github.com>
2021-09-21 22:32:18 +0530

    Create 1043.java

---
+++ 1043.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/partition-array-for-maximum-sum/
+class Solution {
+    int res = 0;
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int dp[] = new int[n];
+       
+        for(int i=0;i<n;i++){
+            int sum = 0, maxNum = 0;
+            for(int j=0;j<k && i-j>=0;j++){
+                if(maxNum < arr[i-j])
+                    maxNum = arr[i-j];
+                
+                int temp = maxNum*(j+1)+((i-j-1>=0)?dp[i-j-1]:0);
+                if(sum < temp)sum = temp;
+            }
+            dp[i] = sum;
+        }
+    return dp[n-1]; 
+    }
+}
+
+
+/*
+T.C - O(n*k)
+S.C - O(n)
+*/
