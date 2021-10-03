0912088e6fb463673ff98d43ecca7eed89e304aa
sush <59618780+sush-101@users.noreply.github.com>
2021-10-03 14:23:15 +0530

    Update 1551.java

--- 1551.java
+++ 1551.java
@@ -1,3 +1,4 @@
+//Approach1: tc - O(n)
 class Solution {
     public int minOperations(int n) {
         int i = 0, j = n-1, res = 0;
@@ -6,6 +7,20 @@ class Solution {
             j--;i++;
         }
         return res;
+    }
+}
+
+//Approach2: simplify the above expression. tc-O(1)
+class Solution {
+    public int minOperations(int n) {
+        int res = sigma(n-1)-2*sigma((n-2)/2);
+        if((n&1)==1){
+            res -= n/2;
+        }
+        return res;
+    }
+    private int sigma(int n){
+        return n*(n+1)/2;
     }
 }
 
