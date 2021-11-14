96b5c992fd242ae711c1bae4178a6560e4b6580f
sush <59618780+sush-101@users.noreply.github.com>
2021-11-14 14:48:26 +0530

    Update 452.java

--- 452.java
+++ 452.java
@@ -10,17 +10,16 @@ class Solution {
     public int findMinArrowShots(int[][] points) {
         Arrays.sort(points, (o1,o2)->{
             if(o1[0] > o2[0])return 1;
-            return -1; //no need to return o1[0]==o2[0] case as 0 because the order does not matter after we handle o1[0]>o2[0] case.
+            return -1;
         });
-        int count = 0, left=points[0][0], right=points[0][1];
+        int count = 0, right=points[0][1];
         
         for(int i=1;i<points.length;i++){
             if(points[i][0] > right){
                 count++;
-                left = points[i][0];right=points[i][1];
+                right=points[i][1];
             }else{
                 right = Math.min(right, points[i][1]);
-                left = points[i][0];
             }
         }
         return count+1;
