91f11c0c1d8cb942b3d82b0f1e5e229360debd20
sush <59618780+sush-101@users.noreply.github.com>
2023-12-22 00:03:29 +0530

    Time: 2 ms (97.43%) | Memory: 54.9 MB (86.82%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,29 +1,12 @@
 class Solution {
-    public int maxScore(int[] cardPoints, int k) {
-        int n = cardPoints.length;
-        int prefixSum[] = new int[n+1];
-        int suffixSum[] = new int[n+1];
-        for(int i=1;i<=n;i++){
-            prefixSum[i] = cardPoints[i-1] + prefixSum[i-1];
+        public int maxScore(int[] C, int K) {
+        int total = 0;
+        for (int i = 0; i < K; i++) total += C[i];
+        int best = total;
+        for (int i = K - 1, j = C.length - 1; i >= 0; i--, j--) {
+            total += C[j] - C[i];
+            best = Math.max(best, total);
         }
-        for(int i=n-1;i>=0;i--){
-            suffixSum[i] = cardPoints[i] + suffixSum[i+1];
-        }
-        
-        int left = 0, right = n-1, score = 0, targetLength = n-k;
-        while(left <= right && (right - left + 1) > targetLength){
-            int leftSum = prefixSum[left+k] - prefixSum[left];
-            int rightSum = suffixSum[right-k+1] - suffixSum[right+1];
-            // System.out.println("k " + k + "leftSum "+ leftSum + " rightSum " + rightSum);
-            if(leftSum > rightSum){
-                score += cardPoints[left];
-                left++;
-            }else{
-                score += cardPoints[right];
-                right--;
-            }
-            k--;
-        }
-        return score;
+        return best;
     }
 }
